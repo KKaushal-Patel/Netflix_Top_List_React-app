3d13b2d608401cfd86a1d47555f83b57f24faf9f
Kaushal Patel <kaushalpatel9376@gmail.com>
2021-03-19 17:48:11 +0530

    First commit

---
+++ src/SeriesData.jsx
@@ -0,0 +1,40 @@
+const data = [
+    {
+        id: 1,
+        aname: 'One Punch Man',
+        imgSrc: 'https://wallpapercave.com/wp/wp6986248.jpg',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/80117291'
+    },
+    {
+        id: 2,
+        aname: 'Death Note',
+        imgSrc: 'https://wallpapercave.com/wp/WuM6aGS.jpg',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/70204970'
+    },
+    {
+        id: 3,
+        aname: 'Dark',
+        imgSrc: 'https://wallpapercave.com/wp/wp4056410.jpg',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/80100172'
+    },
+    {
+        id: 4,
+        aname: 'Stranger Things',
+        imgSrc: 'https://wallpapercave.com/wp/wp4595720.png',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/80057281'
+    },
+    {
+        id: 5,
+        aname: 'Lost In Space',
+        imgSrc: 'https://wallpapercave.com/wp/wp5344519.jpg',
+        title: 'A Netflix Original Series',
+        link: 'https://www.netflix.com/in/title/80104198'
+    },
+
+]
+
+export default data;
